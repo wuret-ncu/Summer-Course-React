4d82ea59065c03d7a29f2a0ea80a56b0095e004b
Nathan <nucktwillieren@gmail.com>
2021-07-14 16:59:10 +0800

    fix bug 1. add br tag to components/single.js 2. fix bug in index.js

--- src/components/SingIn.js
+++ src/components/SingIn.js
@@ -2,24 +2,25 @@ import React from 'react';
 import './SingIn.css';
 
 function Data(props) {
-    return(
+    return (
         <div className="singInData">
             <span>{props.name}: </span>
             <input type="text" />
             <br />
+            <br />
         </div>
     )
-    
+
 }
 
-function SingIn(props){
+function SingIn(props) {
     const dataList = props.name.map(value =>
-        <Data key={value} name={value}/>)
-    return(
+        <Data key={value} name={value} />)
+    return (
         <div className="singIn">
             <div className="singInTitle">
                 <span>Sing In</span>
-                <button onClick={props.onClick}>未註冊</button> 
+                <button onClick={props.onClick}>未註冊</button>
             </div>
             {dataList}
             <button type="submit" className="sumbit">送出</button>
